1d7e07576bb7da3cc96f35c822285ec0d1a784ba
Izzat Faris <frz565@gmail.com>
2020-10-26 13:25:45 +0800

    Changed title bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
     <v-app-bar fixed app dense color="primary">
-      <v-toolbar-title class="white--text">
-        RSS News <v-divider vertical></v-divider>
-        <div class="date">{{ date }}</div>
-      </v-toolbar-title>
+      <v-toolbar-title class="white--text">Sensasi RSS</v-toolbar-title>
       <v-spacer />
+      <div class="date white--text">{{ date }}</div>
     </v-app-bar>
     <v-content>
       <v-container>
